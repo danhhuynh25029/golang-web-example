8045fc6718bd176f13670205643c3639c6e16be6
danhhuynh25029 <danhhuynh250@gmail.com>
2021-10-24 14:52:11 +0700

    update code

--- main.go
+++ main.go
@@ -1,61 +1,42 @@
 package main
 
 import(
-	// "fmt"
+	"fmt"
 	"net/http"
 	"html/template"
 	"encoding/json"
-	"database/sql"
-	"log"
-	_ "github.com/go-sql-driver/mysql"
+	// "database/sql"
+	// "log"
+	"module/models"
+	// _ "github.com/go-sql-driver/mysql"
 
 )
-type Student struct{
-	Id int
-	Name string
+func main(){
+	fmt.Println("Server Running")
+	http.HandleFunc("/home",MyHome)
+	http.HandleFunc("/list",AllStudent)
+	http.HandleFunc("/check",check)
+	http.ListenAndServe(":3000",nil)
+	// s := models.String()
+	// fmt.Println(s)
+	// // getData()
 }
 
-func main(){
-	// fmt.Println("Server Running")
-	// http.HandleFunc("/home",MyHome)
-	http.HandleFunc("/list",AllStudent)
-	http.ListenAndServe(":3000",nil)
-	// getData()
-}
-func getData() []Student{
-	var(
-		id int
-		name string
-	)
-	var listStudent []Student
-	db,err := sql.Open("mysql","root:admin12345@tcp(127.0.0.1:3306)/EmployeeManager")
-	if err != nil{
-		log.Fatal(err)
-	}else{
-		log.Println("Sucessful")
-	}
-	defer db.Close()
-	rows, err := db.Query("select id,name from employee")
-	if err != nil{
-		log.Fatal(err)
-	}
-	defer db.Close()
-	for rows.Next(){
-		err := rows.Scan(&id,&name)
-		if err != nil{
-			log.Fatal(err)
-		}
-		listStudent = append(listStudent,Student{id,name})
-	}
-	return listStudent
-}
 func MyHome(w http.ResponseWriter, r *http.Request){
 	tmpl := template.Must(template.ParseFiles("templates/home.html"))
 	// fmt.Fprint(w,"My home")
-	tmpl.Execute(w,nil)
+	listUser := models.GetAll()
+	tmpl.Execute(w,listUser)
 }
 func AllStudent(w http.ResponseWriter, r *http.Request){
-	
-	listStudent := getData()
-	json.NewEncoder(w).Encode(listStudent)
+	listUser := models.GetAll()
+	json.NewEncoder(w).Encode(listUser)
+}
+func check(w http.ResponseWriter, r *http.Request){
+	if r.Method == "POST"{
+		fmt.Println(r.FormValue("name"))
+	}else{
+		tmpl := template.Must(template.ParseFiles("templates/form.html"))
+		tmpl.Execute(w,nil)
+	}
 }
--- models/dbConnect.go
+++ models/dbConnect.go
@@ -1,5 +1,38 @@
 package models
 
 import(
+	"log"
+	"database/sql"
 	_ "github.com/go-sql-driver/mysql"
 )
+type User struct{
+	Id int
+	Name string
+}
+func GetAll() []User{
+	var(
+		id int
+		name string
+	)
+	var listUser []User
+	db,err := sql.Open("mysql","root:admin12345@tcp(127.0.0.1:3306)/EmployeeManager")
+	if err != nil{
+		log.Fatal(err)
+	}else{
+		log.Println("Sucessful")
+	}
+	defer db.Close()
+	rows, err := db.Query("select id,name from employee")
+	if err != nil{
+		log.Fatal(err)
+	}
+	defer db.Close()
+	for rows.Next(){
+		err := rows.Scan(&id,&name)
+		if err != nil{
+			log.Fatal(err)
+		}
+		listUser = append(listUser,User{id,name})
+	}
+	return listUser
+}
